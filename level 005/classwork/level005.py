44670d090f4fe9bf307f2f90b6567e6ff3b44d39
giogiobezhashvili <giogiobezhashvili@gmail.com>
2025-08-06 10:49:56 +0400

    second commit

---
+++ level 005/classwork/level005.py
@@ -0,0 +1,29 @@
+# debbuging-არის შეცდომების გამოსჭორება ჩვენს კოდში რათა  კოდმა იმუშაოს სწორად 
+# case sensitive -არის დაიდისა და პატარა ასოსი განმასხვავებელი
+my_name= "giorgi"
+my_age= 15
+my_brothername="nika"
+my_brother_age= 14
+my_second_name="bezhashvili"
+
+
+print(my_name)
+print(my_age)
+print(my_brothername)
+print(my_brother_age)
+print(my_second_name)
+
+
+# 2name="gio"          აქ შეცდომაა  გამოყენებული რიცხვი
+# name!="giorgi"       აქქ კი სიმბოლოა გამოყენებული
+
+
+age = 15 
+number= 30
+
+print(age+number)
+print(number-age)
+print(number*age)
+
+
+
---
+++ level 005/homework/level 005.py
@@ -0,0 +1,70 @@
+from turtle import*
+
+# კომენტარები გამოიყენება ადამიანებისთვის რადგან როცა სხვას გადასცემ შენს კოდს გაიგოს რას აკეთებდი,
+# კომენტარების გამოყენება კარგი პრაქტიკაა რადგან თუ დაგავიწყდა რას აკეთებდი ერთი წაკითხვით გაგახსენდება
+
+# ვაკეთებ 10 snake_case ჩემს შესახებ 
+my_name = "gio"
+my_lastname = "bezhashvili"
+my_age = "15"
+my_birth_year = "2009"
+my_birth_month = "octomber"
+my_birth_date = "13"
+my1_brother_name = "nika"
+my2_brother_name = "mate"
+device_tipe1 = "phone"
+device_tipe2 = ""
+
+# დავპრინტე 10 snake_case ჩემს შესახებ
+
+print(my_name)
+print(my_lastname)
+print(my_age)
+print(my_birth_year)
+print(my_birth_month)
+print(my_birth_date)
+print(my1_brother_name)
+print(my2_brother_name)
+print(device_tipe1)
+print(device_tipe2)
+
+# შევასწორე კოდი
+name = "gio"
+age_num = 40
+my_favmov = "blood and bone"
+my_friends_name = "andria"
+  
+print ("My name is" + name)
+print ("my age is", + age_num)
+print ("My Favorite Movie is" + my_favmov)
+print ("My Friend's name is" + my_friends_name)
+    
+#შევქმენი 2 ცვლადი რის შემდეგაც მოვახდინე კონკატენაცია და დავპრინტე ტერმინალში
+
+string1 = "hello group"
+string2 = "73"
+combined = string1 + " " + string2
+print(combined)
+
+first_part = "IT program"
+second_part = "is amazing"
+result = first_part + " " + second_part
+print(result)
+
+word1 = "python is"
+word2 = "convenient language"
+sentence = word1 + " " + word2
+print(sentence)
+   
+start = "i am"
+middle = "learning"
+end = "programming"
+full = start +" "+ middle +" "+ end
+print(full)
+
+num1 = 30 
+num2 = 50 
+total = num1 + num2
+print(total)
+
+# snake_case არის ტექსტის დამწერლობის სტილი სადაც სიტყვები გამოყოფილია ქვედა ხაზით
