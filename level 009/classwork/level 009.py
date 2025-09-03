9e1fce9539985562e2878f7d8ef77870dac2a4f9
giogiobezhashvili <giogiobezhashvili@gmail.com>
2025-09-03 16:12:53 +0400

    second commit

---
+++ level 009/classwork/level 009.py
@@ -0,0 +1,47 @@
+#1
+# and - აბრუნებს true მაშინ როცა ორივე მხარე true არის
+#or აბრუნებს true -ს მაშინაც როცა ერთ მაიც true არის
+
+2
+name = input("enter your name")
+
+if name == "ixvi":
+    print("true")
+
+else: 
+    print("false")
+
+3
+number = int(input("enter name: "))
+
+if number > 50:
+    print("true")
+else:
+    print("false")
+
+#4 
+my_favorite_toy = "lego"
+
+user_toy = input("enter your favorit toy")
+
+if user_toy == my_favorite_toy: 
+    print("true")
+else:
+    print("fals")
+
+#5 
+print(True and False and True or False and True and False and True and False or True and False or True or False)
+
+print(5 > 4)
+print(10 < 8)
+print(7 >= 7)
+print(8 <= 9)
+print(4 == 4)
+
+
+
+
+#7)
+
+print(5 > 4 and 10 > 6)
+print(7 < 4 or 2 == 2)
