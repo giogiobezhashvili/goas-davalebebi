be6a4e3bb80acf13718cc4b82fc85bbe4079427f
giogiobezhashvili <giogiobezhashvili@gmail.com>
2025-09-16 21:22:15 +0400

    second commit

---
+++ level 011/classwork/level 011.py
@@ -0,0 +1,35 @@
+# Flowchart-ნაბიჯ-ნაბიჯ მიყვება
+# Algorithm- არის კონკრეტული ნაბიჯების თანმიმდევრობა
+# Natural Language  არის ჩვეულებრივი ენა
+#  Pseudocode  არის ალგორითმის აღწერა ტექსტური სახით,
+# რომელიც ჰგავს პროგრამირების ენას
+
+
+#  Sequence   კოდში ნაბიჯები შესრულება ერთის შემდეგ მეორე.
+a = 5       
+b = 3       
+c = a + b   
+print(c)
+
+
+# Iteration ნიშნავს მოქმედების გამეორებას მანამ
+for i in range(1, 6):
+    print(i)
+
+# Selection ნიშნავს გადაწყვეტილების მიღებას პირობის მიხედვით.
+
+
+#  5) 
+for i in range(20):
+    print("გიორგი")
+
+
+# 6) 
+for i in range(1, 11):   
+    print(i)
+
+for i in range(1, 51, 2):  
+    print(i)
+
+for i in range(2, 68, 2):  
+    print(i)
---
+++ level 011/homework/level011.py
@@ -0,0 +1,24 @@
+
+
+
+
+# for ციკლი გამოიყენება იმ შემთხვევაში,როდესაც გვინდა რაიმე მოქმედება განმეორდეს რამდენჯერმე.
+
+
+
+for i in range(1, 101):  
+    print(i)
+
+
+
+for i in range(1,100,5):
+    print(i)
+
+for i in range(5, 88):     
+    if i % 2 == 0:      
+        print(i)
+
+name = input("შეიყვანეთ თქვენი სახელი: ")  
+
+for i in range(15):       
+    print(name)
